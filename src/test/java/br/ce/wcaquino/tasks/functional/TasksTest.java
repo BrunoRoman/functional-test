3f06fa789651ba37365c058e9dbb60710d4fb15d
BrunoRoman <74359118+BrunoRoman@users.noreply.github.com>
2021-10-05 09:18:03 -0300

    Update TasksTest.java

--- src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
+++ src/test/java/br/ce/wcaquino/tasks/functional/TasksTest.java
@@ -14,7 +14,7 @@ public class TasksTest {
 	
 	
 	public WebDriver acessarAplicacao() {
-		System.setProperty("webdriver.chrome.driver","/root/bin/chromedriver");
+		System.setProperty("webdriver.chrome.driver","/home/brgonro/chromedriver");
 		WebDriver driver = new ChromeDriver();
 		driver.navigate().to("http://192.168.200.127:8080/tasks");
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
